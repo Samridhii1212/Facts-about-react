0d8802de0fcef9f3c46f5f5510da873d3e47744e
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2023-04-23 20:14:46 +0530

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Navbar(props) {
+    return (
+
+            <nav>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width="200"/>
+                <h3>React</h3>
+                <h4>react project</h4>
+            </nav>
+      
+    );
+
+}
+export default Navbar;
