6e6ed4b01ca07566a332b61428cf6cd5d5575420
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2023-04-23 20:15:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Main() {
+    return (
+        <main>
+            <h1>Facts about React</h1>
+            <ul>
+                <li> React is not a framework</li>
+                <li>React has a strong community</li>
+                <li>virtual DOM is faster than real DOM</li>
+                <li> React development and SEO go together</li>
+            </ul>
+        </main>
+           
+
+    );
+
+}
+export default Main;
