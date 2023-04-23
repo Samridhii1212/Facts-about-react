144759f705805807d3f618f1daf5e4b1d7e54de0
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2023-04-23 20:17:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import reactdom from "react-dom";
+import Facts from "./facts";
+
+
+
+
+reactdom.render(
+    <>
+    <Facts/>
+    </>,
+    document.getElementById("root")
+);
